f015130767d9670ed826966a589409c922087844
Phan Duc <phanminhduc2003@gmail.com>
2023-03-01 17:10:11 +0700

    up file

---
+++ index.js
@@ -0,0 +1,12 @@
+function findMaxNumber(num1, num2){
+    if(num1 > num2){
+      return num1
+    } else {
+      return num2
+    }
+  }
+  console.log ("Num1: 10");
+  console.log("Num2: 5");
+  let checkMax1 = findMaxNumber(10, 5);
+  console.log('Max Number:',checkMax1);
+  
